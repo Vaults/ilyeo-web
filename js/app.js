f719e458ba7cff080da89a9d777a732a5caeb693
Marcel Visser <marcel1337@hotmail.com>
2016-07-10 13:42:09 +0200

    Added PSD for background img, added more comments & fixed duplicate side-bar images

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 /**
  * Created by Marcel on 9-7-2016.
  */
+/**
+ * List of all pages in navigation bar
+ */
 var pageList = {
     'home':         {dutch:'Home',
                         eng:'Home'},
@@ -61,7 +64,7 @@ var ILYEO = angular.module('Ilyeo', ['ui.router']).config(function($urlRouterPro
         if(ctrl){state.controller = ctrl;}
         $stateProvider.state(dutch, state);
     }
-    //Add all file names in pages.html
+    //Add all file names in pages.html and add respective controllers to those states
     initState('besturen', 'boardCtrl');
     initState('exameneisen');
     initState('examens', 'examCtrl');
@@ -71,6 +74,11 @@ var ILYEO = angular.module('Ilyeo', ['ui.router']).config(function($urlRouterPro
     initState('vereniging');
 });
 
+/**
+ * Sets the rootScope's global variables to the respective values.
+ * For instance, the navigation bar page list, whether the site should be displayed in English
+ * and the names of all the images in the img/impression folder
+ */
 ILYEO.run(function($rootScope){
     $rootScope.pageList = pageList;
     $rootScope.ENG = (LANG !== "NL");
@@ -87,6 +95,9 @@ ILYEO.controller('indexCtrl', function($scope, $rootScope){
      */
     $scope.pages = $rootScope.pageList;
     $scope.ENG = $rootScope.ENG;
+    /**
+     * Sets the left bar images to random images from the img/impressions folder
+     */
     function changeImgs(){
         $scope.img1 = 'ilyeosite-1.jpg';
         $scope.img2 = 'ilyeosite-2.jpg';
@@ -94,16 +105,25 @@ ILYEO.controller('indexCtrl', function($scope, $rootScope){
             $scope.img1 = $rootScope.imgList[(Math.random()*$rootScope.imgList.length | 0)];
             $scope.img2 = $rootScope.imgList[(Math.random()*$rootScope.imgList.length | 0)];
         }
-
+        if($scope.img1 == $scope.img2){changeImgs();}
     }
+
+    /**
+     * Ensures that changeImgs gets called on page switch
+     */
     changeImgs();
     $scope.$on('$stateChangeStart', changeImgs);
+
+    /**
+     * Function to change language and reload page
+     * @param ln Language to switch to, 'EN' or 'NL'.
+     */
     $scope.changeLang = function(ln){
         localStorage.setItem('lang', ln);
         location.reload();
     }
 });
-//see js/data/beltexams.js
+//see js/data/beltexams.js, sets scope variables to include the data in that file
 ILYEO.controller('examCtrl', function($scope, $rootScope) {
     $scope.ENG = $rootScope.ENG;
     //Belt codes to name
@@ -111,7 +131,7 @@ ILYEO.controller('examCtrl', function($scope, $rootScope) {
     $scope.belts = BELTS;
     $scope.data = BELT_EXAMS;
 });
-//see js/data/boards.js
+//see js/data/boards.js, sets scope variables to include the data in that file
 ILYEO.controller('boardCtrl', function($scope, $rootScope) {
     $scope.ENG = $rootScope.ENG;
     $scope.roles = ROLES;
--- js/data/impressions.php
+++ js/data/impressions.php
@@ -1,7 +1,9 @@
 <?php
 /**
- * Created by Marcel on 9-7-2016.
+ * Created by Marcel on 9-7-2016. Returns all file names in the images/impression folder.
+ * Unfortunately this is not trivially possible to do in HTML/JS so PHP was chosen.
  */
+
  $arr = scandir('../../images/impression');
  echo json_encode(array_slice($arr,2));
  ?>
